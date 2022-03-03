bf875e3e96e7050e4ab66b032893e7e9bf77c0f8
dhdelima <dh.delima@hotmail.com>
2022-03-03 09:25:11 -0300

    initial commit

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos:[
+                    {url:'politico_3.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos:[
+                    {url: 'politico_4.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '21',
+                nome: 'Carlos Benzeno',
+                partido: 'ABC',
+                vice: 'Benzeno',
+                fotos:[
+                    {url:'politico_5.jpg', legenda: 'Prefeito'},
+                    {url:'politico_6.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '91',
+                nome: 'Leao de Treino',
+                partido: 'DEFG',
+                vice: 'Gato de Jogo',
+                fotos:[
+                    {url: 'politico_1.jpg', legenda: 'Prefeito'},
+                    {url: 'politico_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
